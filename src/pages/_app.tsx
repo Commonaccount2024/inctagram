109690b4b7e745662d6954312c5cfb493e110bdf
MrEvgeniy1989 <mr.evgeniy1989@yandex.ru>
2024-04-30 18:09:20 +0300

    chore: install @commonaccount2024/inctagram-ui-kit

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,6 +5,7 @@ import { Layout } from '@/shared/components/layout/Layout'
 import '@/styles/globals.css'
 import '@fontsource/roboto'
 import '@fontsource-variable/inter'
+import '@commonaccount2024/inctagram-ui-kit/dist/style.css'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
