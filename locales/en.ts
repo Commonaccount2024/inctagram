e35d42eab32d4a3494ab2da6547c2074e86d2482
Alexander Grygoriev <truedevelop@yahoo.com>
2024-05-01 18:54:34 +0300

    feat: internationale features done

---
+++ locales/en.ts
@@ -0,0 +1,20 @@
+import { LocaleType } from "./ru";
+
+export const en: LocaleType = {
+  test: 'Lorem Ipsum is simply dummy text of the printing',
+  singUpPage: {
+    title: 'Sing up page'
+  },
+  singInPage: {
+    title: 'Sing in page'
+  },
+  passwordRecoveryPage: {
+    title: 'Password recovery page'
+  },
+  forgotPasswordPage: {
+    title: 'Forgot password page'
+  },
+  createNewPasswordPage: {
+    title: 'Create new password page'
+  },
+}
---
+++ locales/ru.ts
@@ -0,0 +1,20 @@
+export const ru = {
+  test: 'Lorem Ipsum - это текст-"рыба", часто используемый в печати ',
+  singUpPage: {
+    title: 'Страница регистрации'
+  },
+  singInPage: {
+    title: 'Страница авторизации'
+  },
+  passwordRecoveryPage: {
+    title: 'Страница восстановления пароля'
+  },
+  forgotPasswordPage: {
+    title: 'Страница забыл пароль'
+  },
+  createNewPasswordPage: {
+    title: 'Страница создания нового пароля'
+  },
+}
+
+export type LocaleType = typeof ru;
