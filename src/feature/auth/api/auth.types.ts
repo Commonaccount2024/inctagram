627df11d539fbb2e104985cc1888623dc16f5cce
MrEvgeniy1989 <mr.evgeniy1989@yandex.ru>
2024-05-18 12:22:17 +0300

    refactor: Using prettier and eslint processed the files.

--- src/feature/auth/api/auth.types.ts
+++ src/feature/auth/api/auth.types.ts
@@ -1,4 +1,3 @@
-
 export type LoginParams = {
   email: string
   password: string
@@ -21,7 +20,7 @@ export type ResponseErrorDataType = {
   statusCode: number
 }
 
-export type ResponseSuccessCase = ResponseErrorDataType | void;
+export type ResponseSuccessCase = ResponseErrorDataType | void
 
 export type SendVerificationCode = {
   confirmationCode: string
