b2268cc57232dc2b5139cf6222cd8a99bb4dd3b7
nadzeya <vitserresvit@gmail.com>
2024-05-28 16:47:40 +0300

    stylelint

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,3 +1,12 @@
 module.exports = {
-  extends: '@it-incubator/stylelint-config',
+  extends: ['next/core-web-vitals', '@it-incubator/eslint-config', 'plugin:storybook/recommended'],
+  overrides: [
+    {
+      files: ['**/*.stories.tsx'],
+      rules: {
+        'no-console': 'off',
+        'react-hooks/rules-of-hooks': 'off',
+      },
+    },
+  ],
 }
