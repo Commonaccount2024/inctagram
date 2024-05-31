0ac571fe3b57a33e19047d4dc03352d39f0ccb38
Alexander Grygoriev <truedevelop@yahoo.com>
2024-05-31 23:55:57 +0300

    UC1/IT-2 feat: add modal window when registr form sent

--- src/shared/components/SignUpForm/SignUpModal/SignUpModal.tsx
+++ src/shared/components/SignUpForm/SignUpModal/SignUpModal.tsx
@@ -1,5 +1,5 @@
-
 import { Button, Modal, Typography } from '@commonaccount2024/inctagram-ui-kit'
+
 import s from './SignUpModal.module.scss'
 
 interface LogoutModalProps {
@@ -10,13 +10,15 @@ interface LogoutModalProps {
 
 export const SignUpModal = ({ email, isOpen, onClose }: LogoutModalProps) => {
   return (
-    <>
-      <Modal onOpenChange={onClose} open={isOpen} title={'Email sent'}>
-        <Typography className={s.text} variant={'regular-text-16'}>
-          We have sent a link to confirm your email to {email}
-        </Typography>
-          <Button onClick={onClose}>ok</Button>
-      </Modal>
-    </>
+    <Modal onOpenChange={onClose} open={isOpen} title={'Email sent'}>
+      <Typography className={s.text} variant={'regular-text-16'}>
+        We have sent a link to confirm your email to {email}
+      </Typography>
+      <div className={s.buttonsDiv}>
+        <Button onClick={onClose} variant={'primary'}>
+          ok
+        </Button>
+      </div>
+    </Modal>
   )
 }
